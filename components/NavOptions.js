8a52c19d78595e14aa7daca2a2a477e9e7858ca8
mercylin muthoni <linmercymuthoni@gmail.com>
2023-07-14 17:39:17 +0300

    homescreen options

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -2,6 +2,9 @@ import { FlatList, Image, StyleSheet, Text, TouchableOpacity, View } from 'react
 import React from 'react'
 import tailwind from 'twrnc';
 import { Icon } from '@rneui/themed';
+import { useNavigation } from '@react-navigation/native';
+import { useSelector } from 'react-redux';
+import { selectOrigin } from '../slices/navSlice';
 
 
 const data = [
@@ -20,35 +23,44 @@ const data = [
 ];
 
 const NavOptions = () => {
+    const navigation=useNavigation();
+    const origin= useSelector(selectOrigin);
+
+
   return (
    <FlatList 
-   data={data}
-   keyExtractor={(item) => item.id} //whenever you have a list you have to have a key
-   horizontal
-   renderItem={({item}) => (
-    <TouchableOpacity
-        style={tailwind`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}
-    >
-        <View>
-            <Image 
-            source={{
-                uri: item.image
-            }}
-            style={{
-                width: 120,
-                height: 120,
-                resizeMode: "contain"
-            }} />
+        data={data}
+        keyExtractor={(item) => item.id} //whenever you have a list you have to have a key
+        horizontal
+        renderItem={({item}) => (
+            <TouchableOpacity
+                // disabled={!origin}
+                onPress={() => navigation.navigate(item.screen)}
+                style={tailwind`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}
+            >
+                <View  
+                    // style={tailwind`${!origin && 'opacity-20'}`} 
+                >
+                    <Image 
+                    source={{
+                        uri: item.image
+                    }}
+                    style={{
+                        width: 120,
+                        height: 120,
+                        resizeMode: "contain"
+                    }} />
 
-            <Text style={tailwind`mt-2 font-semibold text-lg`}>
-                {item.title}
-            </Text>
+                    <Text style={tailwind`mt-2 font-semibold text-lg`}>
+                        {item.title}
+                    </Text>
 
-            <Icon  style={tailwind`p-2 bg-black rounded-full w-10 mt-4`} name='arrowright' color="white" type='antdesign'  />
+                    <Icon  style={tailwind`p-2 bg-black rounded-full w-10 mt-4`} name='arrowright' color="white" type='antdesign'  />
 
-        </View>
-    </TouchableOpacity>
-   )} />
+                </View>
+            </TouchableOpacity>
+        )} 
+   />
   )
 }
 
