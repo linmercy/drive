4be783c8d2d8f3c0a2a995d86fa641b2482d836b
mercylin muthoni <linmercymuthoni@gmail.com>
2023-07-14 17:34:54 +0300

    added the eats screen

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { Provider } from 'react-redux/es';
 import { store } from './store';
 import HomeScreen from './screens/HomeScreen';
 import MapScreen from './screens/MapScreen';
+import EatsScreen from './screens/EatsScreen';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -26,6 +27,15 @@ export default function App() {
           <Stack.Screen
             name='MapScreen'
             component={MapScreen}
+            options={{
+              headerShown: false
+            }} 
+            
+          />
+
+          <Stack.Screen
+            name='EatsScreen'
+            component={EatsScreen}
             options={{
               headerShown: false
             }} 
