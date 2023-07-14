ded2579b73d4c93e7424f6c13e8cfe2589b616d9
mercylin muthoni <linmercymuthoni@gmail.com>
2023-07-14 17:40:47 +0300

    homescreen complete navigation

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,8 +2,14 @@ import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native'
 import React from 'react'
 import tw from 'twrnc'
 import NavOptions from '../components/NavOptions'
+import {PLACES_API_KEY} from "@env"
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
+import { useDispatch } from 'react-redux'
+import { setDestination, setOrigin } from '../slices/navSlice'
+import Favourites from '../components/Favourites'
 
 const HomeScreen = () => {
+    const dispatch = useDispatch();
   return (
     <SafeAreaView style={tw`bg-yellow-200 h-full`}>
         <View style={tw`p-5`}>
@@ -18,16 +24,41 @@ const HomeScreen = () => {
                 }}
             />
 
+            <GooglePlacesAutocomplete 
+                nearbyPlacesAPI='GooglePlacesSearch'
+                debounce={400}
+                placeholder='Where From?'
+                styles={{
+                    container: {
+                        flex: 0,
+                    }
+                }}
+                fetchDetails={true}
+                onPress={(data, details = null) => {
+                    dispatch(setOrigin({
+                        location: details.geometry.location,
+                        description: data.description
+                    }))
+
+                    dispatch(setDestination(null))
+                }}
+                returnKeyType={"search"}
+                enablePoweredByContainer={false}
+                minLength={2}
+
+                query={{
+                    key: PLACES_API_KEY,
+                    language: "en"
+                }}
+             
+            />
+
             <NavOptions />
+
+            <Favourites />
         </View>
     </SafeAreaView>
   )
 }
 
 export default HomeScreen
-
-const styles = StyleSheet.create({
-    text: {
-        color: "blue",
-    }
-})
